9e3b36d21ed0dadfa8c046aa2dfb3f44fdbecfd4
Stephen Hu <stephenhu@seismic.com>
2021-07-19 15:46:18 +0800

    2021 07 19

--- PipeReaderUtility/PipeReaderExtension.cs
+++ PipeReaderUtility/PipeReaderExtension.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.IO.Pipelines;
 using System.Text;
 using System.Text.Json;
+using System.Threading.Channels;
+using System.Threading.Tasks;
 
 namespace PipeReaderUtility
 {
@@ -15,6 +17,44 @@ namespace PipeReaderUtility
         private readonly static byte[] _close = Encoding.UTF8.GetBytes("]");
 
         private readonly static byte[] _objectStart = Encoding.UTF8.GetBytes("{");
+
+        public static ChannelReader<T> GetChannelReader<T>(PipeReader pipeReader, BoundedChannelOptions boundedChannelOptions, JsonSerializerOptions jsonSerializerOptions)
+        {
+            var channel = Channel.CreateBounded<T>(boundedChannelOptions);
+            var channelReader = channel.Reader;
+            var channelWriter = channel.Writer;
+            Task.Run(() => FlushToWriterAsync(pipeReader, jsonSerializerOptions, channelWriter));
+            return channelReader;
+        }
+
+        private static async Task FlushToWriterAsync<T>(PipeReader pipeReader, JsonSerializerOptions jsonSerializerOptions, ChannelWriter<T> channelWriter)
+        {
+            var readRes = await pipeReader.ReadAsync();
+            try
+            {
+                do
+                {
+                    if (TryParseMessages<T>(in readRes, jsonSerializerOptions, out var item, out var commited, out var examined))
+                    {
+                        await channelWriter.WriteAsync(item);
+                    }
+
+                    pipeReader.AdvanceTo(commited, examined);
+                    if (readRes.IsCompleted || readRes.IsCanceled)
+                    {
+                        break;
+                    }
+
+                    readRes = await pipeReader.ReadAsync();
+                } while (true);
+
+                channelWriter.Complete();
+            }
+            catch (Exception ex)
+            {
+                channelWriter.Complete(ex);
+            }
+        }
 
         public async static IAsyncEnumerable<T> ChunkProcessMessagesAsync<T>(PipeReader pipeReader, JsonSerializerOptions jsonSerializerOptions)
         {
